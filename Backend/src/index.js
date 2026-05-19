3e4e4595a6fc5baa244f156a9aec061ac67ebde4
lanjarset1awan <lanjarsetiawan3@gmail.com>
2026-05-19 23:23:22 +0700

    set up code for deploy

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -25,9 +25,7 @@ app.use(cors());
 
 app.use(express.json());
 
-// Serve Frontend static files
-app.use(express.static(path.join(__dirname, "../../Frontend-React/dist")));
-app.use(express.static(path.join(__dirname, "../../Frontend")));
+// Redundant static file serving removed for separate Vercel deployment
 
 // TEST DB CONNECTION
 pool.query("SELECT NOW()")
