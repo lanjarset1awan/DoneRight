198837f10221ba80756e69b067c56c4d52c7a846
lanjarset1awan <lanjarsetiawan3@gmail.com>
2026-05-20 01:01:43 +0700

    fix trash bug

--- Frontend/src/pages/AdminDashboard.jsx
+++ Frontend/src/pages/AdminDashboard.jsx
@@ -592,11 +592,11 @@ export default function AdminDashboard({ token, user, onLogout, onNavigateReport
                             </svg>
                           </button>
                            <button
-                             className="btn-delete-icon admin-dash-cat-del-icon"
+                             className="admin-dash-cat-del-icon"
                              onClick={() => handleDeleteCategory(cat.id_categories)}
                              title="Hapus Kategori"
                            >
-                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="admin-dash-cat-svg-icon">
+                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                              </svg>
                            </button>
